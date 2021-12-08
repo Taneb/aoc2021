b9b74ad3a3956374b1e314282fcd76db3a839d4b
Nathan van Doorn <nvd1234@gmail.com>
2021-12-08 22:30:01 +0000

    Far simplify solution for day 8

--- app/day8.hs
+++ app/day8.hs
@@ -1,14 +1,7 @@
 module Main where
 
-import SAT.MiniSat
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Set (Set)
-import qualified Data.Set as S
 import Data.Maybe
-
-import Debug.Trace
 
 part1 :: [[String]] -> Int
 part1 = sum . map f1
@@ -27,80 +20,18 @@ part2 = sum . map f1
   where
     f1 xs = 
       let 
-        (signal, digits) = splitDigits [] xs; cond = isSegment :&&: condition signal 
-      in case solve cond of
-      Nothing -> error "Could not find solution"
-      Just solution ->
-        let assignment = understandSolution1 solution
-        in traceShow assignment $ foldl (\r a -> 10 * r + a) 0 $ map (understandSolution2 assignment) digits
+        (signal, digits) = splitDigits [] xs
+        (d1:d7:d4:d23or5a:d23or5b:d23or5c:d06or9a:d06or9b:d06or9c:d8:[]) = map sort . sortOn length $ signal
+        (d3:[], d2or5a:d2or5b:[]) = partition (d1 `isSubsequenceOf`) [d23or5a, d23or5b, d23or5c]
+        (d9:[], d0or6a:d0or6b:[]) = partition (d4 `isSubsequenceOf`) [d06or9a, d06or9b, d06or9c]
+        (d0:[], d6:[]) = partition (d1 `isSubsequenceOf`) [d0or6a, d0or6b]
+        (d5:[], d2:[]) = partition (`isSubsequenceOf` d6) [d2or5a, d2or5b]
+      in foldl (\r a -> 10 * r + a) 0 $ mapMaybe (\d -> elemIndex (sort d) [d0,d1,d2,d3,d4,d5,d6,d7,d8,d9]) digits
 
     splitDigits :: [String] -> [String] -> ([String], [String])
     splitDigits xs ("|":ys) = (xs, ys)
     splitDigits xs (x:ys) = splitDigits (x:xs) ys
     splitDigits _ _ = error "digits not found"
-
-    getDigits [] = error "digits not found"
-    getDigits ("|":xs) = xs
-    getDigits (_:xs) = getDigits xs
-
-    isSegment :: Formula (Char, Char)
-    isSegment = All [ExactlyOne [Var (x, y) | y <- "abcdefg"] | x <- "abcdefg"]
-
-    two :: [Char] -> Formula (Char, Char)
-    two cs = Some [All (zipWith (curry Var) cs' "cf") | cs' <- permutations cs]
-
-    three :: [Char] -> Formula (Char, Char)
-    three cs = Some [All (zipWith (curry Var) cs' "acf") | cs' <- permutations cs]
-
-    four :: [Char] -> Formula (Char, Char)
-    four cs = Some [All (zipWith (curry Var) cs' "bcdf") | cs' <- permutations cs]
-
-    five :: [Char] -> Formula (Char, Char)
-    five cs = Some $ concat
-      [ [All (zipWith (curry Var) cs' "acdeg") | cs' <- permutations cs]
-      , [All (zipWith (curry Var) cs' "acdfg") | cs' <- permutations cs]
-      , [All (zipWith (curry Var) cs' "abdfg") | cs' <- permutations cs]
-      ]
-
-    six :: [Char] -> Formula (Char, Char)
-    six cs = Some $ concat
-      [ [All (zipWith (curry Var) cs' "abcefg") | cs' <- permutations cs]
-      , [All (zipWith (curry Var) cs' "abdefg") | cs' <- permutations cs]
-      , [All (zipWith (curry Var) cs' "abcdfg") | cs' <- permutations cs]
-      ]
-
-    -- this gives us no information. Maybe I should just remove it
-    seven :: [Char] -> Formula (Char, Char)
-    seven cs = Some [All (zipWith (curry Var) cs' "abcdefg") | cs' <- permutations cs]
-
-    condition1 :: String -> Formula (Char, Char)
-    condition1 s = case length s of
-      2 -> two s
-      3 -> three s
-      4 -> four s
-      5 -> five s
-      6 -> six s
-      _ -> Yes 
-
-    condition :: [String] -> Formula (Char, Char)
-    condition = All . map condition1
-
-    understandSolution1 :: Map (Char, Char) Bool -> Map Char Char
-    understandSolution1 = M.fromList . map fst . filter snd . M.toList
-
-    understandSolution2 :: Map Char Char -> String -> Int
-    understandSolution2 m s = case sort . map (fromJust . flip M.lookup m) $ s of
-      "abcefg" -> 0
-      "cf" -> 1
-      "acdeg" -> 2
-      "acdfg" -> 3
-      "bcdf" -> 4
-      "abdfg" -> 5
-      "abdefg" -> 6
-      "acf" -> 7
-      "abcdefg" -> 8
-      "abcdfg" -> 9
-      _ -> error "unrecognized digit"
 
 main :: IO ()
 main = do
